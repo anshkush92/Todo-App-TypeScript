aa8da70085280d478585b3c40a62aa13922dddea
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-04 15:35:49 +0530

    Created the heading and the backgroudn

---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,13 @@
+// Test -------------------------- Importing the Packages ---------------------------------
+
+// Test -------------------------- Importing the styles / other components ----------------
+
+// Test -------------------------- Structure of Props ----------------------------------
+
+// Test -------------------------- The current component ----------------------------------
+const InputField = () => {
+  return <div>This is Input Field</div>;
+};
+
+// Test -------------------------- Exporting the current component ------------------------
+export default InputField;
