ed149da4197261f8352e9029644ac6be3f35e50d
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-04 15:38:26 +0530

    Updated the initialized project with starting setup

---
+++ src/components/Component.tsx
@@ -0,0 +1,15 @@
+// Test -------------------------- Importing the Packages ---------------------------------
+
+// Test -------------------------- Importing the styles / other components ----------------
+
+// Test -------------------------- Structure of Props ----------------------------------
+
+// Test -------------------------- The current component ----------------------------------
+const Component = () => {
+    return (
+        <div>This is a Component</div>
+    );
+}
+
+// Test -------------------------- Exporting the current component ------------------------
+export default Component;
